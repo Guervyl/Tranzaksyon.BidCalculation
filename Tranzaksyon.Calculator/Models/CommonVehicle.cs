5e52e7a573cd0394c7e35933709e58881a01bb8d
Guervyl <duval.guerby@yahoo.fr>
2024-07-18 20:40:00 -0500

    Create the API project, the calculator project and the test project. Create the models Create the tests

---
+++ Tranzaksyon.Calculator/Models/CommonVehicle.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tranzaksyon.Calculator.Models
+{
+    public class CommonVehicle: Vehicle
+    {
+    }
+}
---
+++ Tranzaksyon.Test/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace Tranzaksyon.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
